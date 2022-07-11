1716c4693ab1bc9d67e1155a3fafd955cdba7f8c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-11 22:15:42 +0530

    add a sql query to create a table name contacts

--- dbscript.sql
+++ dbscript.sql
@@ -5,3 +5,18 @@ create table users (
   password varchar(255) not null,
   created_at datetime
 );
+
+create table contacts(
+    id int primary key auto_increment,
+    firstname varchar(50) not null,
+    lastname varchar(50),
+    email varchar(255) not null unique,
+    phone varchar(100) not null unique,
+    avatar varchar(255),
+    address varchar(255),
+    city varchar(100),
+    pincode varchar(10),
+    state varchar(50),
+    country varchar(50),
+    created_at datetime,
+);
