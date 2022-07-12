1327ff836a4c69c96f0f500ad3d43e032b1e83b6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 13:49:18 +0530

    add a new class Contact.java inside the package com.dev.phonebook.entity

---
+++ src/main/java/com/dev/phonebook/entity/Contact.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 12-07-2022/07/2022
+ *   Time: 01:49 PM
+ *   File: Contact
+ */
+
+package com.dev.phonebook.entity;
+
+public class Contact {
+}
