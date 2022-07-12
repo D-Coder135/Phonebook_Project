a0549fd5d2556afd1c8b897418d55904205e671b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 14:51:42 +0530

    add an @Options annotation

--- src/main/java/com/dev/phonebook/dao/UserDao.java
+++ src/main/java/com/dev/phonebook/dao/UserDao.java
@@ -1,16 +1,13 @@
 package com.dev.phonebook.dao;
 
 import com.dev.phonebook.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 public interface UserDao {
 
     //CRUD
     @Insert("insert into users (name, email, password, created_at) values (#{name}, #{email}, #{password}, #{createdAt})")
-
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     public void createUser(User user);
 
     //Queries
