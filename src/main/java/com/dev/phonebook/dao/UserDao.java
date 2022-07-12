79486260e1ef11e62558a1b1188688a15aa25dfd
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 14:46:12 +0530

    add an abstract method getUserByEmail() of type User

--- src/main/java/com/dev/phonebook/dao/UserDao.java
+++ src/main/java/com/dev/phonebook/dao/UserDao.java
@@ -1,8 +1,11 @@
 package com.dev.phonebook.dao;
 
+import com.dev.phonebook.entity.User;
+
 public interface UserDao {
 
     //CRUD
 
     //Queries
+    public User getUserByEmail(String email);
 }
