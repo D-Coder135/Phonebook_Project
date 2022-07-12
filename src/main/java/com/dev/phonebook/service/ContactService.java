a8b1958b32a9647347559ccb414e3a1a876b7255
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 14:25:17 +0530

    add a new class ContactService.java inside the package com.dev.phonebook.service

---
+++ src/main/java/com/dev/phonebook/service/ContactService.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 12-07-2022/07/2022
+ *   Time: 02:25 PM
+ *   File: ContactService
+ */
+
+package com.dev.phonebook.service;
+
+public class ContactService {
+}
