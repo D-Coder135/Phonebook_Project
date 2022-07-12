66360cd71b7d8a9d96e090fffc1d6f8dcf1ffa4b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 14:24:51 +0530

    add a new class UserService.java inside the package com.dev.phonebook.service

---
+++ src/main/java/com/dev/phonebook/service/UserService.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 12-07-2022/07/2022
+ *   Time: 02:24 PM
+ *   File: UserService
+ */
+
+package com.dev.phonebook.service;
+
+public class UserService {
+}
