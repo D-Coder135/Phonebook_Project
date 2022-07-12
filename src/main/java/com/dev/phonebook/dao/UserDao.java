66ea2cfd32f256eee167a540c4dba35b29f7cf92
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 14:47:46 +0530

    add the @Results annotation along with the @Result annotation inside it

--- src/main/java/com/dev/phonebook/dao/UserDao.java
+++ src/main/java/com/dev/phonebook/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.dev.phonebook.dao;
 
 import com.dev.phonebook.entity.User;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 public interface UserDao {
@@ -9,5 +11,8 @@ public interface UserDao {
 
     //Queries
     @Select("Select * from users where email = #{email}")
+    @Results({
+            @Result(column = "created_at", property = "createdAt")
+    })
     public User getUserByEmail(String email);
 }
