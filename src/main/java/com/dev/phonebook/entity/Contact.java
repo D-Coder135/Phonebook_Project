4b95246b70197c32c693b40129fde4827175c7a2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 13:50:47 +0530

    initialize private fields of types Integer, String and Date

--- src/main/java/com/dev/phonebook/entity/Contact.java
+++ src/main/java/com/dev/phonebook/entity/Contact.java
@@ -7,5 +7,19 @@
 
 package com.dev.phonebook.entity;
 
+import java.util.Date;
+
 public class Contact {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String avatar;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String pincode;
+    private Date createAt;
 }
