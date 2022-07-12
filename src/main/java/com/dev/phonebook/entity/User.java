73eff1434af92b70f021bca3423e7e3b2c11b9cb
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 13:45:42 +0530

    add a new class User.java inside the package com.dev.phonebook.entity

---
+++ src/main/java/com/dev/phonebook/entity/User.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 12-07-2022/07/2022
+ *   Time: 01:45 PM
+ *   File: User
+ */
+
+package com.dev.phonebook.entity;
+
+public class User {
+}
