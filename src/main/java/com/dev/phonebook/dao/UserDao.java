8cfab72ea99ba8d9ff7edc4d2c45ed5a0472f83d
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 14:46:57 +0530

    add the @Select annotation along with a query inside it

--- src/main/java/com/dev/phonebook/dao/UserDao.java
+++ src/main/java/com/dev/phonebook/dao/UserDao.java
@@ -1,11 +1,13 @@
 package com.dev.phonebook.dao;
 
 import com.dev.phonebook.entity.User;
+import org.apache.ibatis.annotations.Select;
 
 public interface UserDao {
 
     //CRUD
 
     //Queries
+    @Select("Select * from users where email = #{email}")
     public User getUserByEmail(String email);
 }
