8472c0f040421c4a79e282df52f630557c801cf1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 13:54:12 +0530

    add a new class DaoFactory.java inside the package com.dev.phonebook.dao

---
+++ src/main/java/com/dev/phonebook/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 12-07-2022/07/2022
+ *   Time: 01:53 PM
+ *   File: DaoFactory
+ */
+
+package com.dev.phonebook.dao;
+
+public class DaoFactory {
+}
