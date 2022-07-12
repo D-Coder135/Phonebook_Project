5d512a5f9a1ab742418fa144bb0b72961f7461be
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-12 14:51:15 +0530

    add an @Insert annotation along with a sql query inside it

--- src/main/java/com/dev/phonebook/dao/UserDao.java
+++ src/main/java/com/dev/phonebook/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.dev.phonebook.dao;
 
 import com.dev.phonebook.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
@@ -8,6 +9,8 @@ import org.apache.ibatis.annotations.Select;
 public interface UserDao {
 
     //CRUD
+    @Insert("insert into users (name, email, password, created_at) values (#{name}, #{email}, #{password}, #{createdAt})")
+
     public void createUser(User user);
 
     //Queries
