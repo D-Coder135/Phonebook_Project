cbd6e0351137cb7373f6fc995f44b1f64f091c5c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 15:20:53 +0530

    remove the doPost() method

--- src/main/java/com/dev/phonebook/web/DashboardServlet.java
+++ src/main/java/com/dev/phonebook/web/DashboardServlet.java
@@ -11,9 +11,4 @@ public class DashboardServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
