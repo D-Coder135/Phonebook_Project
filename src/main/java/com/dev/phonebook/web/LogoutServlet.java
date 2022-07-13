43de35c011a63a0ffd19f621d0da892b950440ff
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 22:02:55 +0530

    call the response.sendRedirect() method

--- src/main/java/com/dev/phonebook/web/LogoutServlet.java
+++ src/main/java/com/dev/phonebook/web/LogoutServlet.java
@@ -10,5 +10,6 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getSession().invalidate();
+        response.sendRedirect("./login");
     }
 }
