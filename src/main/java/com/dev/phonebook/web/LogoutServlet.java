b9879f1cd97d869672a0b9a2b77faa986e05ae23
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 22:02:34 +0530

    call the getSession() method followed by invalidate method

--- src/main/java/com/dev/phonebook/web/LogoutServlet.java
+++ src/main/java/com/dev/phonebook/web/LogoutServlet.java
@@ -9,6 +9,6 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getSession().invalidate();
     }
 }
