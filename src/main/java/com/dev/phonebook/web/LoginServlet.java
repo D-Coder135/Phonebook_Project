c5359b596d4d4c124d9bdab2944ddf347e226385
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 21:12:36 +0530

    initialize a User object with the getSession() method call followed by getAttribute() method call

--- src/main/java/com/dev/phonebook/web/LoginServlet.java
+++ src/main/java/com/dev/phonebook/web/LoginServlet.java
@@ -1,5 +1,7 @@
 package com.dev.phonebook.web;
 
+import com.dev.phonebook.entity.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,6 +11,6 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        User user = (User) request.getSession().getAttribute("user");
     }
 }
