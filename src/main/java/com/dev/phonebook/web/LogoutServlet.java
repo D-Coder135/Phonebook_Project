41282162be3eee0380619182cff8b5585fb702e2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 22:01:23 +0530

    remove the doPost() method

--- src/main/java/com/dev/phonebook/web/LogoutServlet.java
+++ src/main/java/com/dev/phonebook/web/LogoutServlet.java
@@ -11,9 +11,4 @@ public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
