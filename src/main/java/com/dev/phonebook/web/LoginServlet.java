96c85b4dd8529c3ee75544be1dd0970f2d605af0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 21:38:38 +0530

    override the doPost() method

--- src/main/java/com/dev/phonebook/web/LoginServlet.java
+++ src/main/java/com/dev/phonebook/web/LoginServlet.java
@@ -19,4 +19,9 @@ public class LoginServlet extends HttpServlet {
 
         request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
