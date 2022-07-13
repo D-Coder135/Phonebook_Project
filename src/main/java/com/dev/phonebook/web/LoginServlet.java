822d32165b62b6ffd45b054b3f1d886420c06b6f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 21:39:51 +0530

    initialize an object of type UserService

--- src/main/java/com/dev/phonebook/web/LoginServlet.java
+++ src/main/java/com/dev/phonebook/web/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.dev.phonebook.web;
 
 import com.dev.phonebook.entity.User;
+import com.dev.phonebook.service.UserService;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -24,5 +25,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String email = req.getParameter("email");
         String password = req.getParameter("password");
+
+        UserService service = new UserService();
     }
 }
