72d2d460e9edfb09b55ddcdcf4063625ce7573ed
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 15:21:18 +0530

    update the value attribute for the @WebServlet annotation

--- src/main/java/com/dev/phonebook/web/DashboardServlet.java
+++ src/main/java/com/dev/phonebook/web/DashboardServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "DashboardServlet", value = "/DashboardServlet")
+@WebServlet(name = "DashboardServlet", value = "/dashboard")
 public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
