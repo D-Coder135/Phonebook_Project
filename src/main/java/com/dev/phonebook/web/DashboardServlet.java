50fbfe8a53bad8a3629e0a45e1dfc11fee020a6c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 15:22:20 +0530

    add an if() block along with a condition inside it

--- src/main/java/com/dev/phonebook/web/DashboardServlet.java
+++ src/main/java/com/dev/phonebook/web/DashboardServlet.java
@@ -12,5 +12,10 @@ public class DashboardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = (User) request.getSession().getAttribute("user");
+
+        if (user == null) {
+            response.sendRedirect("./");
+            return;
+        }
     }
 }
