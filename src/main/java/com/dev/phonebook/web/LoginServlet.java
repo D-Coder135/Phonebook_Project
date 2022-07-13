a8ffd5c7ae625051e967275242cae57a42852618
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-13 21:09:58 +0530

    update the value attribute for the @WebServlet annotation

--- src/main/java/com/dev/phonebook/web/LoginServlet.java
+++ src/main/java/com/dev/phonebook/web/LoginServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+@WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
