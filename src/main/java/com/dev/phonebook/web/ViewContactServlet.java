560e72f8a9751f2059f90dcca43420322702ee69
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-14 21:27:35 +0530

    initialize a field of type String

--- src/main/java/com/dev/phonebook/web/ViewContactServlet.java
+++ src/main/java/com/dev/phonebook/web/ViewContactServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class ViewContactServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String input = request.getParameter("id");
     }
 
     @Override
