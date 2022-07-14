262e2088f9714b6234da26f33163a0bd98c9b89c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-14 16:08:21 +0530

    add the @Insert annotation along with the sql query inside it as a parameter

--- src/main/java/com/dev/phonebook/dao/ContactDao.java
+++ src/main/java/com/dev/phonebook/dao/ContactDao.java
@@ -1,11 +1,15 @@
 package com.dev.phonebook.dao;
 
 import com.dev.phonebook.entity.Contact;
+import org.apache.ibatis.annotations.Insert;
 
 public interface ContactDao {
 
     // CRUD
 
+    @Insert("insert into contacts (firstname, lastname, email, phone, avatar, address, city, state, pincode, country, user_id, created_at)" +
+            " values (#{firstname}, #{lastname}, #{email}, #{avatar}, #{address}, #{city}, #{state}, #{pincode}, #{country}," +
+            " #{userId}, #{createdAt})")
     void createContact(Contact c);
 
     // QUERIES
