7cef25b9dcb0854d02efa0ec884e0cbd073599f0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-14 16:03:41 +0530

    add some comment along with some reformatting

--- src/main/java/com/dev/phonebook/dao/ContactDao.java
+++ src/main/java/com/dev/phonebook/dao/ContactDao.java
@@ -3,9 +3,15 @@ package com.dev.phonebook.dao;
 import com.dev.phonebook.entity.Contact;
 
 public interface ContactDao {
+
+    // CRUD
+
+    void createContact(Contact c);
+
+    // QUERIES
+
     Contact getContactByEmail(String email);
 
     Contact getContactByPhone(String phone);
 
-    void createContact(Contact c);
 }
