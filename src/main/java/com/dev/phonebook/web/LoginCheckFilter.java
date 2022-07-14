28c5caad26918f63679ca57602eb94dc7982188a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-14 14:46:06 +0530

    update the urlPatterns attribute of the @WebFilter annotation

--- src/main/java/com/dev/phonebook/web/LoginCheckFilter.java
+++ src/main/java/com/dev/phonebook/web/LoginCheckFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(filterName = "LoginCheckFilter", urlPatterns = {"/dashboard"})
+@WebFilter(filterName = "LoginCheckFilter", urlPatterns = {"/dashboard", "/add-contact"})
 public class LoginCheckFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
     }
