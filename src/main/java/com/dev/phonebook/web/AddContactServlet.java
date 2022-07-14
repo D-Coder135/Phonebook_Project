a12440e2f7a9dc81a6e1617da3db3cce39e6a56e
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-14 14:25:36 +0530

    update the value attribute for the @WebServlet annotation

--- src/main/java/com/dev/phonebook/web/AddContactServlet.java
+++ src/main/java/com/dev/phonebook/web/AddContactServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "AddContactServlet", value = "/AddContactServlet")
+@WebServlet(name = "AddContactServlet", value = "/add-contact")
 public class AddContactServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
