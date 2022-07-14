f9658ca524700938fb2f5e2ea43242c0a297d9de
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-14 14:12:47 +0530

    initialize fields of type HttpServletRequest and HttpServletResponse

--- src/main/java/com/dev/phonebook/web/LoginCheckFilter.java
+++ src/main/java/com/dev/phonebook/web/LoginCheckFilter.java
@@ -2,6 +2,8 @@ package com.dev.phonebook.web;
 
 import javax.servlet.*;
 import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(filterName = "LoginCheckFilter")
@@ -14,6 +16,7 @@ public class LoginCheckFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
     }
 }
