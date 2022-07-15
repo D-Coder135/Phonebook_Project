ed48ddd4bc069c77429920b0b380ecf5190510ba
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-15 22:36:46 +0530

    paste some part of the code

--- src/main/java/com/dev/phonebook/web/EditUpdateContactServlet.java
+++ src/main/java/com/dev/phonebook/web/EditUpdateContactServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "EditUpdateContactServlet", value = "/edit-and-update-contact")
 public class EditUpdateContactServlet extends HttpServlet {
@@ -30,6 +32,33 @@ public class EditUpdateContactServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String avatar = request.getParameter("avatar");
+        String address = request.getParameter("address");
+        String city = request.getParameter("city");
+        String state = request.getParameter("state");
+        String pincode = request.getParameter("pincode");
+        String country = request.getParameter("country");
 
+        Contact c = new Contact();
+        c.setFirstname(firstname);
+        c.setLastname(lastname);
+        c.setEmail(email);
+        c.setPhone(phone);
+        c.setAvatar(avatar);
+        c.setAddress(address);
+        c.setCity(city);
+        c.setState(state);
+        c.setPincode(pincode);
+        c.setCountry(country);
+
+        User user = (User) request.getSession().getAttribute("user");
+        c.setUserId(user.getId());
+
+        ContactService service = new ContactService();
+        Map<String, String> errors = new HashMap<>();
     }
 }
