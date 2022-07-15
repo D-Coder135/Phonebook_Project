f68f200fa53bbe09fcf8e84af497de08acb4b82f
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-15 12:42:14 +0530

    add a new servlet ViewAllContactsServlet.java inside the com.dev.phonebook.web package

---
+++ src/main/java/com/dev/phonebook/web/ViewAllContactsServlet.java
@@ -0,0 +1,19 @@
+package com.dev.phonebook.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ViewAllContactsServlet", value = "/ViewAllContactsServlet")
+public class ViewAllContactsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
