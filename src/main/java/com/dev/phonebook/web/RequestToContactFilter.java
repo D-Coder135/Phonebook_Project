7299335b428f79cb3f2e54d9ca02f0b1d2ddb171
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-15 23:40:15 +0530

    paste some part of the code inside the RequestToContactFilter.java

--- src/main/java/com/dev/phonebook/web/RequestToContactFilter.java
+++ src/main/java/com/dev/phonebook/web/RequestToContactFilter.java
@@ -1,5 +1,7 @@
 package com.dev.phonebook.web;
 
+import com.dev.phonebook.entity.Contact;
+
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +20,30 @@ public class RequestToContactFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
+
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String email = request.getParameter("email");
+        String phone = request.getParameter("phone");
+        String avatar = request.getParameter("avatar");
+        String address = request.getParameter("address");
+        String city = request.getParameter("city");
+        String state = request.getParameter("state");
+        String pincode = request.getParameter("pincode");
+        String country = request.getParameter("country");
+
+        Contact c = new Contact();
+        c.setFirstname(firstname);
+        c.setLastname(lastname);
+        c.setEmail(email);
+        c.setPhone(phone);
+        c.setAvatar(avatar);
+        c.setAddress(address);
+        c.setCity(city);
+        c.setState(state);
+        c.setPincode(pincode);
+        c.setCountry(country);
+
         chain.doFilter(request, response);
     }
 }
