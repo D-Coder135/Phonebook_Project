bd7fe23258ab9f9e61603343af9f274b66ab87a1
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-16 21:13:43 +0530

    add an if() block along with a condition inside it

--- src/main/java/com/dev/phonebook/service/ContactService.java
+++ src/main/java/com/dev/phonebook/service/ContactService.java
@@ -93,5 +93,10 @@ public class ContactService {
     public void deleteContactById(Integer id, Integer userId, Map<String, String> errors) throws IOException {
         ContactDao dao = DaoFactory.getContactDao();
         Contact c1 = dao.getContact(id, userId);
+
+        if (c1 == null) {
+            errors.put("userId", "Permission denied for deleting this contact");
+            return;
+        }
     }
 }
