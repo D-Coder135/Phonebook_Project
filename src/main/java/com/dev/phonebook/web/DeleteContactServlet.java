89cae92302036a588d6eaa7f7957bf8d24430243
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-07-16 21:18:57 +0530

    initialize a String field

--- src/main/java/com/dev/phonebook/web/DeleteContactServlet.java
+++ src/main/java/com/dev/phonebook/web/DeleteContactServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class DeleteContactServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String input = request.getParameter("id");
     }
 
     @Override
